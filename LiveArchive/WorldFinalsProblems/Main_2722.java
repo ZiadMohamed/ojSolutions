76ce2435003f0380c875e3494fac078fc08fefd4
ziad.mohamed <ziad_mohamed9473@hotmail.com>
2015-12-23 20:55:03 +0200

    There was no need for dp

--- LiveArchive/WorldFinalsProblems/Main_2722.java
+++ LiveArchive/WorldFinalsProblems/Main_2722.java
@@ -11,92 +11,43 @@ import java.util.StringTokenizer;
 public class Main_2722 { 
 	// class name should be changed to "Main" before submitting on LiveArchive
 
-	static int memo[][][], sz,sz2;
-	static boolean a[],b[];
-	static BigInteger ans,pow,zero,one,two;
+	static int sz,sz2;
+	static boolean a[],b[],arr[];
+	static BigInteger ans,pow,two;
 	
-	public static boolean solve(int ind,int prev,int change){
-		if(ind==-1)return (change==0);
-		
-		
-		if(memo[ind][prev][change]!=-1)
-			return (memo[ind][prev][change]==1);
-		
-		
-		boolean initial=a[ind];
-		if(prev==1)initial=!initial;
-		
-		if(change==1){
-			if(solve(ind-1,1,(initial==b[ind])?1:0)){
-				memo[ind][prev][change]=1;
-				return true;
+	public static boolean solve(boolean f){
+		boolean change=false,prev=false;
+		for(int i=sz-1;i>=0;--i){
+			boolean initial=a[i];
+			if(prev)initial=!initial;
+			
+			if(change){
+				arr[i]=prev=true;
+				change=(initial==b[i]);
+				continue;
 			}
-			memo[ind][prev][change]=0;
-			return false;
-		}
-		
-		if(initial!=b[ind]){
-			if(solve(ind-1,0,1) || (ind==sz-1 && solve(ind-1,1,0))){
-				memo[ind][prev][change]=1;
-				return true;
+			
+			if(initial!=b[i]){
+				if(!f && i==sz-1){
+					arr[i]=prev=true;
+					change=false;
+				}
+				else{
+					arr[i]=prev=false;
+					change=true;
+				}
 			}
-		}
-		else{
-			if(solve(ind-1,0,0) || (ind==sz-1 && solve(ind-1,1,1))){
-				memo[ind][prev][change]=1;
-				return true;
-			}
-		}
-		
-		
-		
-		memo[ind][prev][change]=0;
-		return false;
-	}
-	
-	
-	public static void build(int ind,int prev,int change){
-		if(ind==-1)return;
-		
-		
-		
-		boolean initial=a[ind];
-		if(prev==1)initial=!initial;
-		
-		if(change==1){
-			if(solve(ind-1,1,(initial==b[ind])?1:0)){
-				ans=ans.add(pow);
-				pow=pow.divide(two);
-				build(ind-1,1,(initial==b[ind]?1:0));
-				return;
-			}
-			return;
-		}
-		
-		if(initial!=b[ind]){
-			if(solve(ind-1,0,1)){
-				pow=pow.divide(two);
-				build(ind-1,0,1);
-			}
-			else if(ind==sz-1 && solve(ind-1,1,0)){
-				ans=ans.add(pow);
-				pow=pow.divide(two);
-				build(ind-1,1,0);
+			else{
+				if(!f && i==sz-1)
+					arr[i]=prev=change=true;
+				
+				else{
+					arr[i]=prev=false;
+					change=false;
+				}
 			}
 		}
-		else{
-			if(solve(ind-1,0,0)){
-				pow=pow.divide(two);
-				build(ind-1,0,0);
-			}
-			else if(ind==sz-1 && solve(ind-1,1,1)){
-				ans=ans.add(pow);
-				pow=pow.divide(two);
-				build(ind-1,1,1);
-			}
-		}
-
-		
+		return !change;
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -104,15 +55,13 @@ public class Main_2722 {
 		PrintWriter writer = 
 				new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 		
-		memo=new int[335][2][2];
+
 		a=new boolean[335];
 		b=new boolean[335];
+		arr=new boolean[335];
 		
-		
-		zero = new BigInteger("0");
-		one = new BigInteger("1");
-		two = new BigInteger("2");
-		
+
+		two=new BigInteger("2");
 		
 		BigInteger x,y;
 		int cs=1;
@@ -121,7 +70,7 @@ public class Main_2722 {
 			x=new BigInteger(inp.nextToken());
 			y=new BigInteger(inp.nextToken());
 
-			if(x.compareTo(zero)==0 && y.compareTo(zero)==0)break;
+			if(x.compareTo(BigInteger.ZERO)==0 && y.compareTo(BigInteger.ZERO)==0)break;
 			
 			
 			sz=0;
@@ -132,32 +81,32 @@ public class Main_2722 {
 			
 			while(sz<sz2)a[sz++]=false;
 			while(sz2<sz)b[sz2++]=false;
-			
-			
-			
-			for(int i=0;i<335;++i)
-				for(int j=0;j<2;++j)
-					for(int k=0;k<2;++k)
-						memo[i][j][k]=-1;
+
 			
 			if(cs>1)writer.println();
 			writer.print("Case Number "+cs+": ");
 			++cs;
 			
-			if(solve(sz-1,0,0)){
-				ans = new BigInteger("0");
-				pow = two.pow(sz-1);
-				build(sz-1,0,0);
-				writer.println(ans);
+			ans = BigInteger.ZERO;
+			pow = BigInteger.ONE;
+			
+			if(!solve(true)){
+				if(!solve(false)){
+					writer.println("impossible");
+					continue;
+				}
+				else toDec();
 			}
-			else writer.println("impossible");
+			else toDec();
+			
+			writer.println(ans);
 		}
 		writer.close();
 	}
 	
 	public static void toBin(BigInteger x,boolean f){
-		while(x.compareTo(zero)==1){
-			if(x.remainder(two).compareTo(one)==0){
+		while(x.compareTo(BigInteger.ZERO)==1){
+			if(x.remainder(two).compareTo(BigInteger.ONE)==0){
 				if(f)a[sz++]=true;
 				else b[sz2++]=true;
 			}
@@ -166,6 +115,13 @@ public class Main_2722 {
 				else b[sz2++]=false;
 			}
 			x=x.divide(two);
+		}
+	}
+	
+	public static void toDec(){
+		for(int i=0;i<sz;++i){
+			if(arr[i])ans=ans.add(pow);
+			pow=pow.multiply(two);
 		}
 	}
 
